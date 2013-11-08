b93fefe1dfd9eb7a3ea410b4f0b17b72e43d20b0
gabrielalejandrodevoto <gabrielalejandrodevoto@gmail.com>
2013-11-08 12:13:10 +0000

    -Arreglo del buil fallido del servidor de integracion continua.

---
+++ test/modelotest/EliminarCuandoSeHayanCreadoTests.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.ejemplo1;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class EliminarCuandoSeHayanCreadoTests {
+
+	@Test
+	public void test(){
+        Assert.assertTrue(true);
+	}
+
+}
