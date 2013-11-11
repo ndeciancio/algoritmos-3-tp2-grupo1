83c9396e7146c1dc6be466539e2946c196bc3a8f
santiagomonsech <santiagomonsech@gmail.com>
2013-11-11 22:00:51 +0000

    .Los metodos que estaban en movil se mudaron a jugador. .movil queda librado para implementar con la interaccion con el evento

--- src/modelo/juego/Jugador.java
+++ src/modelo/juego/Jugador.java
@@ -1,14 +1,74 @@
 package modelo.juego;
 
-//import modelo.general.Mapa;
+import modelo.general.Mapa;
+import modelo.general.Posicion;
+import modelo.moviles.Movil;
+import modelo.evento.Evento;
 
 public class Jugador {
 
 	//	private Mapa mapaDeJuego = Mapa.getInstance();
 	private Integer movimientos;
+	private Movil	vehiculo;
+	private Mapa unaMapa = Mapa.getInstance();
+	private Posicion posicionActual;
+	private Posicion posicionAnterior;
 	
+	public Jugador(Posicion posicionInicial, Movil vehiculo){
+        this.movimientos = 0;
+        this.posicionActual = posicionInicial;
+        this.posicionAnterior = new Posicion(posicionInicial);
+        setVehiculo(vehiculo);		
+	}
+	
+    public Movil getVehiculo(){
+    	return this.vehiculo;
+    }
+    
+    public Posicion getPosicionActual(){
+    	return this.posicionActual;
+    }
+    
+    public Integer getMovimientos(){
+    	return this.movimientos;
+    }
+    
     public void sumarMovimientos(int movimientos){
         this.movimientos += movimientos;
+    }
+    
+    public void setVehiculo(Movil vehiculo){
+    	this.vehiculo = vehiculo;
+    }
+    
+	public void moverDerecha(){
+        recordarPosicion();
+        posicionActual.setx(posicionActual.getCoordenadaX() + 2);
+	}
+	
+	public void moverIzquierda(){
+		recordarPosicion();
+        posicionActual.setx(posicionActual.getCoordenadaX() - 2);
+	}
+
+	public void moverArriba(){
+        recordarPosicion();
+        posicionActual.sety(posicionActual.getCoordenadaY() + 2);
+	}
+
+	public void moverAbajo(){
+        recordarPosicion();
+        posicionActual.sety(posicionActual.getCoordenadaY() - 2);
+	}
+	
+    private void recordarPosicion(){
+        posicionAnterior.setx(posicionActual.getCoordenadaX());
+        posicionAnterior.sety(posicionActual.getCoordenadaY());
+    }
+
+    public void volverAtras(){
+        this.posicionAnterior.setx(posicionAnterior.getCoordenadaX());
+        this.posicionAnterior.sety(posicionAnterior.getCoordenadaY());
     }
 
 }
--- src/modelo/moviles/CuatroXCuatro.java
+++ src/modelo/moviles/CuatroXCuatro.java
@@ -1,10 +1,6 @@
 package modelo.moviles;
 
-import modelo.general.Posicion;
 
 public final class CuatroXCuatro extends Movil {
 
-		public CuatroXCuatro(Posicion posicionInicial){
-			super(posicionInicial);
-		}
 }
