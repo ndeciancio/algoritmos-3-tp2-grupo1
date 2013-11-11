931ae2ba4741e003390613ed8d6c402f07167977
santiagomonsech@gmail.com <santiagomonsech@gmail.com@d2c3c317-2e60-4bf0-35a8-2aacb4e48841>
2013-11-11 22:00:51 +0000

    .Los metodos que estaban en movil se mudaron a jugador. .movil queda librado para implementar con la interaccion con el evento

--- trunk/src/modelo/moviles/Automovil.java
+++ trunk/src/modelo/moviles/Automovil.java
@@ -1,11 +1,7 @@
 package modelo.moviles;
 
-import modelo.general.Posicion;
 
 public final class Automovil extends Movil {
 	
-	public Automovil(Posicion posicionInicial){
-		super(posicionInicial);
-	}
 
 }
--- trunk/src/modelo/moviles/Moto.java
+++ trunk/src/modelo/moviles/Moto.java
@@ -1,11 +1,6 @@
 package modelo.moviles;
 
-import modelo.general.Posicion;
 
 public final class Moto extends Movil {
 	
-	public Moto(Posicion posicionInicial){
-		super(posicionInicial);
-	}
-
 }
