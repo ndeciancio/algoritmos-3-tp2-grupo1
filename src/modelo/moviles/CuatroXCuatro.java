9d4dc247cbde28b889e9c3cb06da20eec23c5d47
adrian.mb.1 <adrian.mb.1@hotmail.com>
2013-11-11 22:14:58 +0000

    Móviles implementados.

--- src/modelo/moviles/CuatroXCuatro.java
+++ src/modelo/moviles/CuatroXCuatro.java
@@ -1,6 +1,25 @@
 package modelo.moviles;
 
+import modelo.juego.Jugador;
 
-public final class CuatroXCuatro extends Movil {
+public final class CuatroXCuatro implements Movil {
+    
+    @Override
+    public void cruzarBache(Jugador jugador){
+        jugador.sumarMovimientos(1);
+    }
+    
+    @Override
+    public void cruzarControlPolicial(Jugador jugador){   
+        jugador.sumarMovimientos(1);
+        if(Math.random() < 0.5)
+            jugador.sumarMovimientos(3);
+    }
+    
+    @Override
+    public void cruzarPiquete(Jugador jugador){
+        jugador.volverAtras();
+    }
+
 
 }
--- src/modelo/moviles/Moto.java
+++ src/modelo/moviles/Moto.java
@@ -1,6 +1,24 @@
 package modelo.moviles;
 
+import modelo.juego.Jugador;
 
-public final class Moto extends Movil {
-	
+public final class Moto implements Movil {
+
+    @Override
+    public void cruzarBache(Jugador jugador){
+        jugador.sumarMovimientos(4);
+    }
+    
+    @Override
+    public void cruzarControlPolicial(Jugador jugador){
+        jugador.sumarMovimientos(1);
+        if(Math.random() < 0.8)
+            jugador.sumarMovimientos(3);
+    }
+    
+    @Override
+    public void cruzarPiquete(Jugador jugador){
+        jugador.sumarMovimientos(3);
+    }
+    
 }
