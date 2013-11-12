d00ccc202a83edabf7739933bc25eca1f5e370f8
adrian.mb.1 <adrian.mb.1@hotmail.com>
2013-11-12 00:16:37 +0000

    Sorpresas que pide el enunciado.

---
+++ src/modelo/evento/sorpresas/MovimientosAdicionales.java
@@ -0,0 +1,18 @@
+package modelo.evento.sorpresas;
+
+import modelo.general.Posicion;
+import modelo.juego.Jugador;
+
+public class MovimientosAdicionales extends Sorpresa {
+    
+    public MovimientosAdicionales(Posicion posicion) {
+        super(posicion);
+    }
+
+    @Override
+    public void interactuarCon(Jugador jugador) {
+        int movimientosSumados = jugador.getMovimientos() / 4;
+        jugador.sumarMovimientos(movimientosSumados);
+    }
+
+}
