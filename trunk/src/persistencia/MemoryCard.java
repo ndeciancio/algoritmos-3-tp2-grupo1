b8e3c4cf71acdb7f8507ecd3514d9864ab31eed1
adrian.mb.1@hotmail.com <adrian.mb.1@hotmail.com@d2c3c317-2e60-4bf0-35a8-2aacb4e48841>
2013-11-16 20:22:06 +0000

    Persistencia de puntuaciones implementada (lectura y escritura). Falta la lectura y escritura de mapas y estados de partida. No se esfuercen mucho por entender el código ahora, solo vean las dos últimas pruebas en MemoryCardTest y el jueves lo charlamos, e implementamos lo que falta. Si hay que volver atras solo borramos el paquete persistencia.

---
+++ trunk/src/persistencia/MemoryCard.java
@@ -0,0 +1,58 @@
+package persistencia;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class MemoryCard {
+
+	File archivo;
+	DocumentBuilder builder;
+	Document documento;
+	
+	public TablaDePuntuaciones cargarPuntuaciones(File archivo){
+		abrirArchivo(archivo);
+		ManagerDePuntuaciones lector = new ManagerDePuntuaciones(documento);
+		return lector.cargarPuntuaciones();
+	}
+	
+	public void nuevaPuntuacion(File archivo, String nombre, int movimientos){
+		abrirArchivo(archivo);
+		ManagerDePuntuaciones escritor = new ManagerDePuntuaciones(documento);
+		try {
+			escritor.ingresarPuntuacion(archivo, nombre, movimientos);
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void abrirArchivo(File archivo){
+		this.archivo = archivo;
+		construirDocumento();
+	}
+	
+	private void construirDocumento(){
+		try {
+			crearDocumentBuilder();
+			parsearDocumento();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void crearDocumentBuilder() throws ParserConfigurationException{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		builder = factory.newDocumentBuilder();
+	}
+	
+	private void parsearDocumento() throws SAXException, IOException {
+		documento = builder.parse(archivo);
+	}
+	
+}
---
+++ trunk/test/persistencia/tests/MemoryCardTest.java
@@ -0,0 +1,77 @@
+package persistencia.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import persistencia.MemoryCard;
+import persistencia.TablaDePuntuaciones;
+
+public class MemoryCardTest {
+
+	private File archivoPuntuaciones;
+	
+	@Rule
+	public TemporaryFolder temp = new TemporaryFolder();
+	
+	@Before
+	public void setUp() throws IOException {
+		archivoPuntuaciones = temp.newFile("puntuaciones.xml");
+		setPuntuaciones();
+	}
+	
+	private void setPuntuaciones() throws IOException {
+		PrintWriter writer = new PrintWriter(archivoPuntuaciones, "UTF-8");
+		writer.println("<puntuaciones>");
+		writer.println("<jugador nombre=\"Fulano\" movimientos=\"8\" />");
+		writer.println("<jugador nombre=\"Mengano\" movimientos=\"12\" />");
+		writer.println("<jugador nombre=\"Pepito\" movimientos=\"16\" />");
+		writer.println("</puntuaciones>");
+		writer.close();
+	}
+	
+	@Test
+	public void test_archivoTemporalDePuntuacionesPreparadoCorrectamente() throws IOException {
+		BufferedReader lector = new BufferedReader(new FileReader(archivoPuntuaciones));
+		assertEquals(lector.readLine(), "<puntuaciones>");
+		assertEquals(lector.readLine(), "<jugador nombre=\"Fulano\" movimientos=\"8\" />");
+		assertEquals(lector.readLine(), "<jugador nombre=\"Mengano\" movimientos=\"12\" />");
+		assertEquals(lector.readLine(), "<jugador nombre=\"Pepito\" movimientos=\"16\" />");
+		assertEquals(lector.readLine(), "</puntuaciones>");
+		lector.close();
+	}
+	
+	@Test
+	public void test_puntuacionesCargadasCorrectamente(){
+		MemoryCard memory = new MemoryCard();
+		TablaDePuntuaciones puntuaciones = memory.cargarPuntuaciones(archivoPuntuaciones);
+		
+		assertEquals(puntuaciones.entradaEnPosicion(0).getNombre(), "Fulano");
+		assertEquals(puntuaciones.entradaEnPosicion(1).getNombre(), "Mengano");
+		assertEquals(puntuaciones.entradaEnPosicion(2).getNombre(), "Pepito");
+		
+		assertEquals(puntuaciones.entradaEnPosicion(0).getMovimientos(), 8);
+		assertEquals(puntuaciones.entradaEnPosicion(1).getMovimientos(), 12);
+		assertEquals(puntuaciones.entradaEnPosicion(2).getMovimientos(), 16);
+	}
+	
+	@Test
+	public void test_nuevasPuntuacionesSonIngresadasCorrectamente(){
+		MemoryCard memory = new MemoryCard();
+		memory.nuevaPuntuacion(archivoPuntuaciones, "Juancito", 14);
+		
+		TablaDePuntuaciones puntuaciones = memory.cargarPuntuaciones(archivoPuntuaciones);
+		assertEquals(puntuaciones.entradaEnPosicion(2).getNombre(), "Juancito");
+		assertEquals(puntuaciones.entradaEnPosicion(2).getMovimientos(), 14);
+	}
+
+}
