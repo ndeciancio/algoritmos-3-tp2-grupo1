bb2709d28ce9a4368fbe1d1b6b4ac7adee419ca7
hmk142142@gmail.com <hmk142142@gmail.com@d2c3c317-2e60-4bf0-35a8-2aacb4e48841>
2013-11-26 21:59:05 +0000

    agregados controladores y botones para moverse y un label con la cantidad de movimientos

---
+++ trunk/src/controladores/ControladorBotonMoverArriba.java
@@ -0,0 +1,28 @@
+package controladores;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+import vista.componentes.FramePrincipal;
+import modelo.juego.Juego;
+import modelo.juego.Jugador;
+
+public class ControladorBotonMoverArriba implements ActionListener {
+    
+    JFrame framePrincipal;
+
+    public ControladorBotonMoverArriba(FramePrincipal framePrincipal){
+        this.framePrincipal = framePrincipal;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        Juego juego = Juego.getInstance();
+        Jugador jugador = juego.getJugador();
+        jugador.moverArriba();
+        framePrincipal.repaint();
+        framePrincipal.revalidate();        
+    }
+    
+}
