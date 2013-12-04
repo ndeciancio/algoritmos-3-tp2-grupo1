becd798a25e161660dbdb447524ccff1d00937de
nicodec89 <nicodec89@gmail.com>
2013-12-04 04:14:39 +0000

    Bueno basicamente es una boludes, solo le agregue un sonido a los eventos para darle un poco mas de estetica al programa. Veran que importe la libreria JLayer que es la que se encarga de la reproduccion de formato mp3 en java. Agregue la clase ControladorSonido que es la que se encarga de procesar el archivo de sonido.

--- src/modelo/evento/obstaculos/Piquete.java
+++ src/modelo/evento/obstaculos/Piquete.java
@@ -1,37 +1,24 @@
 package modelo.evento.obstaculos;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import controladores.ControladorSonido;
 
 import modelo.evento.Evento;
 import modelo.general.Posicion;
 import modelo.juego.Jugador;
 
-import javazoom.jl.player.Player;
 
 public class Piquete extends Obstaculo {
     
-    Player player;
+    private ControladorSonido player= new ControladorSonido("Sonidos/Enemy Sighted.mp3");
     
     public Piquete(Posicion posicion){
         super(posicion);
-        try { 
-            FileInputStream fis = new FileInputStream("Sonidos/Enemy Sighted.mp3"); 
-            BufferedInputStream bis = new BufferedInputStream(fis); 
-            player = new Player(bis); 
-            } 
-            catch (Exception e) { 
-            System.out.println("Problem playing file " + "Sonidos/Enemy Sighted.mp3"); 
-            System.out.println(e); 
-            } 
     }
 
     @Override
     public void interactuarCon(Jugador jugador){
         jugador.getMovil().cruzarPiquete(jugador);
-        try { player.play(); } 
-        catch (Exception e) { System.out.println(e); } 
-        
+        this.player.play();
     }
     
     @Override
